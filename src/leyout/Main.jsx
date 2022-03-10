83c8d3eef0a5acfaa9be6260b172740d4354ce5c
Vitya <vit9popov@gmail.com>
2022-03-10 18:41:31 +0200

    updte

--- src/leyout/Main.jsx
+++ src/leyout/Main.jsx
@@ -1,15 +1,10 @@
 import React from 'react'
 import {Movies} from '../components/Movies'
-<<<<<<< HEAD
 import {Preloader} from '../components/Preloader'
-=======
-import {Preloader} from '../components/preloader'
->>>>>>> 43cd07c751583dba207a079c5bf4ba3c695ca849
 import {Search} from '../components/Search'
 
 
 export default class Main extends React.Component {
-<<<<<<< HEAD
   constructor(props) {
     super(props)
     this.state = { 
@@ -27,32 +22,17 @@ export default class Main extends React.Component {
     fetch(`http://www.omdbapi.com/?apikey=a74de10&s=${search}`)
      .then(response => response.json())
      .then(data => this.setState({movies: data.Search}))
-=======
-  state = { 
-    movies: [],
-  }
-
-  componentDidMount() { 
-    fetch('http://www.omdbapi.com/?apikey=a74de10&s=matrix')
-    .then(response => response.json())
-    .then(data => this.setState({movies: data.Search}))
->>>>>>> 43cd07c751583dba207a079c5bf4ba3c695ca849
   }
   
   render () {
     const {movies} = this.state
     
     return <main className="conteiner content">
-<<<<<<< HEAD
         <Search SearchMovie={this.SearchMovie} />
         {movies.length ? (
         <Movies movie={this.state.movies} />
       ) : <Preloader />}
         
-=======
-        <Search />
-        {this.movies ? <Preloader /> : <Movies movie={this.state.movies} /> }
->>>>>>> 43cd07c751583dba207a079c5bf4ba3c695ca849
     </main>
   };
 }
